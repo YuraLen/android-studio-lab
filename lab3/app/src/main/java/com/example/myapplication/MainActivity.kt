9718e8e18784b5a68d0823640fd1287e40ca176c
YuraLen <yurii260345@gmail.com>
2025-12-04 22:49:03 +0200

    did counter

--- lab3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lab3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,68 @@
-package com.example.myapplication
+package com.example.androidapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.TextView
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+
+    private var scoreTeamA = 0
+    private var scoreTeamB = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    /** TEAM A METHODS */
+    fun addThreeForTeamA(view: android.view.View) {
+        scoreTeamA += 3
+        displayScoreA()
+    }
+
+    fun addTwoForTeamA(view: android.view.View) {
+        scoreTeamA += 2
+        displayScoreA()
+    }
+
+    fun addOneForTeamA(view: android.view.View) {
+        scoreTeamA++
+        displayScoreA()
+    }
+
+    /** TEAM B METHODS */
+    fun addThreeForTeamB(view: android.view.View) {
+        scoreTeamB += 3
+        displayScoreB()
+    }
+
+    fun addTwoForTeamB(view: android.view.View) {
+        scoreTeamB += 2
+        displayScoreB()
+    }
+
+    fun addOneForTeamB(view: android.view.View) {
+        scoreTeamB++
+        displayScoreB()
+    }
+
+    /** RESET BUTTON */
+    fun resetScore(view: android.view.View) {
+        scoreTeamA = 0
+        scoreTeamB = 0
+        displayScoreA()
+        displayScoreB()
+    }
+
+    /** DISPLAY RESULTS */
+    private fun displayScoreA() {
+        val scoreViewA = findViewById<TextView>(R.id.team_a_score)
+        scoreViewA.text = scoreTeamA.toString()
+    }
+
+    private fun displayScoreB() {
+        val scoreViewB = findViewById<TextView>(R.id.team_b_score)
+        scoreViewB.text = scoreTeamB.toString()
     }
 }
