fcb94059b870597be32dfd34a1d4a0cd20637af3
YuraLen <yurii260345@gmail.com>
2025-12-04 19:24:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttonClick)
+        val text = findViewById<TextView>(R.id.textHello)
+
+        text.text = ""
+
+        button.setOnClickListener {
+            text.text = "Hello Yura Len"
+        }
+    }
+}
